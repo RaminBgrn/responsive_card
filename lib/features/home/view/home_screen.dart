111886973a7508fddff15b8ff4f29a1ac35ac8be
RaminBgrn <ramin.bagheriian@gmail.com>
2025-01-26 15:42:38 +0330

    home page has been create : start to implement responsive framwork

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_card/constants/custom_colors.dart';
+import 'package:responsive_card/features/card/view/card_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Responsive Card',
+          style: GoogleFonts.lato(
+            fontSize: 16,
+            color: myGrey[600],
+          ),
+        ),
+      ),
+      body: Center(
+        child: CardItem(),
+      ),
+    );
   }
 }
